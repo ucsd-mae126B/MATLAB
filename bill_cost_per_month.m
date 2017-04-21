a2c2faa8b11324f926571b2da6ff31a86e606c53
rkosec <rosekosec@gmail.com>
2017-04-21 15:52:20 -0700

    Add files via upload
    Files that are going to be updated with new costs

---
+++ bill_cost_per_month.m
@@ -0,0 +1,6 @@
+cost=[4372 4189 4206 4901 7850 7045 7854 7870 7225 6162 3671 4165];
+month=[1:12];
+bar(month,cost);
+xlabel('Month Number');
+ylabel('Bill Cost ($)');
+title('Meter 6698967: Bill Cost by Month for 2013-2014');
